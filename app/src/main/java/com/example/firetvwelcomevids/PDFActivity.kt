82624f52d5732346278123e5c3c2df00a48780a6
gvnmcg <37965084+gvnmcg@users.noreply.github.com>
2023-08-22 16:39:02 -0600

    PDF Viewer, Loads Stream.

--- app/src/main/java/com/example/firetvwelcomevids/PDFActivity.kt
+++ app/src/main/java/com/example/firetvwelcomevids/PDFActivity.kt
@@ -4,21 +4,48 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.FragmentActivity
 import com.github.barteksc.pdfviewer.PDFView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.BufferedInputStream
+import java.net.HttpURLConnection
+import java.net.URL
+// ...
+
 
 
 class PDFActivity : FragmentActivity() {
+    var pdfView: PDFView? = null;
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdf)
-        val pdfView: PDFView = findViewById(R.id.pdfView)
-
+        pdfView = findViewById(R.id.pdfView)
 //        val movie = intent.getSerializableExtra(DetailsActivity.MOVIE, Movie::class.java)
         val movie = intent.getSerializableExtra(DetailsActivity.MOVIE) as Movie?
-        // Load a PDF file from a file path, URL, or other sources
-        pdfView.fromAsset(movie?.videoUrl) // Change to your PDF file source
-            .defaultPage(0)
-            .spacing(10)
-            .load()
+        movie?.videoUrl?.let { loadPdfFromUrl(it) }
+    }
+    private fun loadPdfFromUrl(pdfUrl: String) {
+        GlobalScope.launch(Dispatchers.IO) {
+            val pdfBytes = fetchPdfBytes(pdfUrl)
+            withContext(Dispatchers.Main) {
+                displayPdf(pdfBytes)
+            }
+        }
+    }
+    private fun fetchPdfBytes(pdfUrl: String): ByteArray {
+        val url = URL(pdfUrl)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.connect()
+        val inputStream = BufferedInputStream(connection.inputStream)
+        return inputStream.readBytes()
+    }
+    private fun displayPdf(pdfBytes: ByteArray) {
+        pdfView?.fromBytes(pdfBytes)?.load()
+    }
+
+    companion object {
+        private const val TAG = "PdfActivity"
     }
 }
